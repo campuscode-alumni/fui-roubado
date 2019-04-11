f8fade19a2b4be5bcee7f7056d4f6fb3bf56fb5e
AndreKanamura <kanamura@gmail.com>
2019-04-11 16:33:45 -0300

    Added items to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,23 +3,28 @@ city1 = City.create!(name: 'São Paulo')
 City.create!(name: 'Rio de Janeiro')
 user = User.create!(email: 'vini@email.com', password: '123456',
                    name: 'Vini', nickname: 'Vini22', city: city1, cpf: '096.509.350-61')
-ItemType.create!(name: 'Notebook')
+item_type3 = ItemType.create!(name: 'Notebook')
 item_type1 = ItemType.create!(name: 'Equipamento fotográfico')
 item_type2 = ItemType.create!(name: 'Aparelho celular')
-brand1 = Brand.create!(name: 'Apple', item_type: item_type1)
+brand1 = Brand.create!(name: 'Apple', item_type: item_type2)
 brand2 = Brand.create!(name: 'Samsung',item_type: item_type2)
+brand3 = Brand.create!(name: 'Minolta',item_type: item_type1)
 item1 = Item.create!(title: 'Celular da mãe', item_type: item_type2,
             status: :stolen, user: user, registry_number: '234567',
             brand: brand1, model: 'DD-00410', color: 'Branco',
             description: 'Tem um rachadura na tela.')
 item2 = Item.create!(title: 'Celular da vó', item_type: item_type2,
             status: :stolen, user: user, registry_number: '123456',
-            brand: brand2, model: 'XT-00010', color: 'Azul',
+            brand: brand1, model: 'XT-00010', color: 'Azul',
             description: 'Tem um adesivo de coração na tampa.')
 item3 = Item.create!(title: 'Camera antiga', item_type: item_type1,
             status: :owned, user: user, registry_number: '891011',
-            brand: brand2, model: 'Canon t3i', color: 'Preto',
-            description: 'Tem um adesivo de coração na tampa.')
+            brand: brand3, model: 'Minolta', color: 'Preto',
+            description: 'Tem marcas de uso no corpo.')
+item4 = Item.create!(title: 'Notebook Macbook', item_type: item_type3,
+            status: :owned, user: user, registry_number: '5377347',
+            brand: brand1, model: 'Macbook Pro', color: 'Branco',
+            description: 'Adesivo fanboy na tampa.')
 report1 = Report.create!(user: user, description: 'Assalto no ponto de ônibus',
                ocurrance_date: '2017-04-03 22:40:50',
                address: 'Praça da Sé, São Paulo, SP', items:[item1])
